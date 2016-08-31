220dbb4559664fd16d9b857dbe2f1640e8bfbeda
Benjamin Swineford <bmswine@gmail.com>
2016-08-31 20:02:43 +0000

    changes

--- server.js
+++ server.js
@@ -1,84 +1,35 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
+var express = require("express"),
+    app = express(),
+    compression = require('compression'),
+    fs = require("fs");
+var port = (process.env.PORT || '3000');
+var ip = (process.env.IP || '0.0.0.0');
+var bodyParser = require('body-parser');
 
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
+app.use(compression());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'pug');
 
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
 
-    sockets.push(socket);
+app.use(express.static(__dirname, {
+    maxAge: 2592000000
+}));
 
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
+app.use('/api', require('./server/database'));
 
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
 
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
+app.get('/', renderIndex);
+function renderIndex(req, res) {
+    res.location('/');
+    res.render('index');
 }
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
-});
+app.use("*", function (req, res) {
+        res.status(404).send('404');
+    });
+app.listen(port, ip);
+console.log('Listening on port ' + port + '...');
