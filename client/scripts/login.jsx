80d2577e75e8944146ebec2dda4ddee4dc12890c
Swinejamin <benjamin@swineford.me>
2016-09-04 07:47:22 -0500

    added firebase login functionality to login screen

--- client/scripts/login.jsx
+++ client/scripts/login.jsx
@@ -3,20 +3,45 @@
 (function () {
     "use strict";
     var LoginForm = React.createClass({
+        getInitialState: function () {
+            return {email: '1@2.3', password: ''};
+        },
+        handleEmailChange: function (e) {
+            this.setState({email: e.target.value});
+        },
+        handlePasswordChange: function (e) {
+            this.setState({password: e.target.value});
+        },
+        handleSubmit: function (e) {
+            e.preventDefault();
+
+            var email = this.state.email.trim(),
+                password = this.state.password.trim();
+            console.log(email);
+            firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // ...
+                console.log(errorCode + ': ' + errorMessage);
+            });
+        },
         render: function () {
             return (
-                <form className="form-signin">
+                <form className="form-signin" onSubmit={this.handleSubmit}>
                     <h2 className="form-signin-heading">Please sign in</h2>
                     <div className="form-group">
                         <label htmlFor="inputEmail" className="sr-only">Email address</label>
                         <input type="email" id="inputEmail" className="form-control" placeholder="Email address"
                                required=""
-                               autoFocus=""/>
+                               autoFocus=""
+                               onChange={this.handleEmailChange}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="inputPassword" className="sr-only">Password</label>
                         <input type="password" id="inputPassword" className="form-control" placeholder="Password"
-                               required=""/>
+                               required=""
+                               onChange={this.handlePasswordChange}/>
                     </div>
                     <div className="form-group">
                         <div className="checkbox pull-left">
