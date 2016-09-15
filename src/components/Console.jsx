d51da340748b868b511b786451b782e062ba56ba
Benjamin <benjamin@swineford.me>
2016-09-15 08:17:41 -0500

    fixed layout for console

--- src/components/Console.jsx
+++ src/components/Console.jsx
@@ -2,39 +2,39 @@
 
 import React from 'react';
 import base from '../modules/rebase';
-import ReactDOM from 'react-dom';
 import RecipeAdder from './RecipeAdder.jsx';
 import IngredientAdder from './IngredientAdder.jsx';
 import TagAdder from './TagAdder.jsx';
 import UnitAdder from './UnitAdder.jsx';
 
 import Paper from 'material-ui/Paper';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 
 const Console = React.createClass({
 
     render() {
         return (
-            <Paper>
-                <Card>
-                    <CardHeader title='Add ingredients, tags, or units to the master list'/>
-                    <CardText>
-                        <IngredientAdder/>
-                    </CardText>
-                    <CardText>
-                        <TagAdder/>
-                    </CardText>
-                    <CardText>
-                        <UnitAdder/>
-                    </CardText>
-                </Card>
-                <Card>
-                    <CardHeader title='Add a recipe to the database'/>
-                    <CardText>
-                        <RecipeAdder/>
-                    </CardText>
-                </Card>
-            </Paper>
+            <div className="console-wrapper">
+                <Paper >
+                    <Toolbar>
+                        <ToolbarGroup>
+                            <ToolbarTitle text="Add ingredients, tags, or units to the master list"/>
+                        </ToolbarGroup>
+                    </Toolbar>
+                    <IngredientAdder/>
+                    <TagAdder/>
+                    <UnitAdder/>
+                </Paper>
+
+                <Paper>
+                    <Toolbar>
+                        <ToolbarGroup>
+                            <ToolbarTitle text="Add a recipe to the database"/>
+                        </ToolbarGroup>
+                    </Toolbar>
+                    <RecipeAdder/>
+                </Paper>
+            </div>
         );
     }
 });
