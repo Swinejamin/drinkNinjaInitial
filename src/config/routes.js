2d90714fa678546a69784c5a27c8081d881a68ba
Benjamin <benjamin@swineford.me>
2016-09-23 14:11:33 -0500

    simplified console view

--- src/config/routes.js
+++ src/config/routes.js
@@ -65,13 +65,7 @@ render((
         <Route path="/" component={App}>
             <IndexRoute component={Dashboard} onEnter={requireAuth}/>
             <Route path="dashboard" component={Dashboard} onEnter={requireAuth}/>
-            <Route path="console" component={Console} onEnter={auth.requireAdmin}>
-                <Route path="recipes" component={RecipeBrowser}/>
-                <Route path="ingredients" component={IngredientAdder}/>
-                <Route path="tags" component={TagAdder}/>
-                <Route path="units" component={UnitAdder}/>
-                <Route path="users" component={UserEditor}/>
-            </Route>
+            <Route path="console" component={Console} onEnter={auth.requireAdmin} />
             <Route path="suggestions" component={Suggestions} onEnter={requireAuth}/>
             <Route path="recipe/:key" component={RecipeDetailView}/>
             <Route path="about" component={AboutView}/>
