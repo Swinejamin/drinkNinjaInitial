4e918efe74e08c7d5be04be244870efa33ce51c1
Benjamin Swineford <benjamin@swineford.me>
2017-04-04 10:13:22 -0500

    updated to use firebase admin

--- server/database.js
+++ server/database.js
@@ -1,17 +1,19 @@
 var express = require("express"),
     router = express.Router(),
-    firebase = require('firebase');
+    firebase = require('firebase'),
+    admin = require('firebase-admin');
 
-firebase.initializeApp({
-    serviceAccount: "server/drinkme-key.json",
+var serviceAccount = require('./drinkme-key.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://drinkme-6efd3.firebaseio.com"
 });
-var db = firebase.database();
-var auth = firebase.auth();
+var db = admin.database();
+var auth = admin.auth();
 var user = null;
 
 router.post('/user/:idToken', setUser);
-
 
 
 module.exports = router;
@@ -21,7 +23,7 @@ module.exports = router;
 function setUser(req, res) {
     user = null;
     var idToken = req.params.idToken;
-    firebase.auth().verifyIdToken(idToken)
+    admin.auth().verifyIdToken(idToken)
         .then(function (decodedToken) {
             user = decodedToken.uid;
             res.json({
